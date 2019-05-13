d368378a3448c0458b26c27680c45adb9835d04b
Rok <roksolar999@gmail.com>
2019-05-13 15:54:55 +0200

    Retrieval fix, query processing

--- indexer/retrieval.py
+++ indexer/retrieval.py
@@ -1,5 +1,28 @@
 import sqlite3
+from text_processor import get_words
 
+def get_results(query):
+    conn = sqlite3.connect('../inverted-index.db')
+    c = conn.cursor()
+    sql = '''    
+        SELECT p.documentName AS docName, SUM(frequency) AS freq, GROUP_CONCAT(indexes) AS idxs
+        FROM Posting p
+        WHERE
+            p.word IN ({seq})
+        GROUP BY p.documentName
+        ORDER BY freq DESC;'''.format(seq=','.join(['?']*len(query)))
+    cursor = c.execute(sql, query)
+    for row in cursor:
+        print("\tHits: %d\n\t\tDoc: '%s'\n\t\tIndexes: %s" % (row[1], row[0], row[2]))
+
+query = ['test.,','in', 'spot']
+print(query)
+query = get_words(query)
+print(query)
+get_results(['test','spot'])
+
+
+'''
 def get_doc(word):
     conn = sqlite3.connect('../inverted-index.db')
     c = conn.cursor()
@@ -10,22 +33,23 @@ def get_doc(word):
     conn.close()
     return rows
 
-query = ["sistem","spot"]
+
 results = {}
 for word in query:
-    rows = get_doc(word)
-    for row in rows:
-         if row[1] in results:
-            freq = row[2]
-            results[row[1]][0] += freq
-            results[row[1]][1].append(row)
-         else:
-            freq = row[2]
-            results[row[1]] = [freq,[row]]
+   rows = get_doc(word)
+   for row in rows:
+        if row[1] in results:
+           freq = row[2]
+           results[row[1]][0] += freq
+           results[row[1]][1].append(row)
+        else:
+           freq = row[2]
+           results[row[1]] = [freq,[row]]
 
 sorted_results = sorted(results.values(), key=lambda kv: kv[0],reverse=True)
 
 for  data in sorted_results:
-    print(data)
+   print(data)
 
 
+'''
